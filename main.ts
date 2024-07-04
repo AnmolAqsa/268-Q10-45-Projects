d2c3f22e4d56a3b3088a83fadfafb43b99c3530a
AnmolAqsa <165940283+AnmolAqsa@users.noreply.github.com>
2024-07-04 12:28:41 -0700

    Add files via upload
    Q10, 45 Projects

---
+++ main.ts
@@ -0,0 +1,17 @@
+/*
+Q10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write
+because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence
+describing what the program does.
+*/
+
+//Name = Aqsa Mughal
+//Date = 14.06.2024
+//Program = Javascript
+
+console.log("javascript");
+
+//This program is about additional operator
+
+let number = 55;
+let number2 = 45;
+console.log(number+number2);
